760cce6055be0ed603966a61d5b04bbda00bb3a4
Mathew <mathew.titus@outlook.com>
2023-10-30 11:13:49 -0400

    starting to add server to configuration

--- sshkey.sh
+++ sshkey.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-echo "ssh username"
+# Prompt for SSH username
+echo "Enter your SSH username:"
 read target
-# Get the username
 
-echo "ssh IP"
+# Prompt for remote server's IP address or hostname
+echo "Enter the remote server's IP or hostname:"
 read IP
-# Get the IP address
 
-$remote= "$target@$IP"
-#Combines them into an ssh login format
+# Combine username and IP to form the remote address
+remote="$target@$IP"
 
+# Generate an SSH key pair
 ssh-keygen -t ed25519 -f ~/.ssh/$target
+
+# Copy the public key to the remote server
 ssh-copy-id -i ~/.ssh/$target $remote
+
+# Add the private key to your SSH agent
 ssh-add ~/.ssh/$target
+
+echo "SSH key setup completed."
+
 #uses variables to generate and send an ssh key to the remote endpoint, then adds the key to the Local endpoint.
